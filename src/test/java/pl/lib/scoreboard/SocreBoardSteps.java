b0fb8a8f94c7421cc81162591ff32d3fded75db1
Michał Kącki <mkacki77@gmail.com>
2025-02-02 21:59:43 +0100

    Test implementation - tests failing

--- src/test/java/pl/lib/scoreboard/SocreBoardSteps.java
+++ src/test/java/pl/lib/scoreboard/SocreBoardSteps.java
@@ -1,6 +1,7 @@
 package pl.lib.scoreboard;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -8,10 +9,12 @@ import io.cucumber.java.en.Then;
 
 public class SocreBoardSteps {
 
-    String home;
-    String away;
+    private String home;
+    private String away;
+    private Match match;
 
-    Match match;
+    private ScoreBoard scoreBoard;
+    private Exception thrownException;
 
     @Given("Two teams {string} and {string}")
     public void selectTeams(String home, String away) {
@@ -27,6 +30,33 @@ public class SocreBoardSteps {
     @Then("current result of that match is {string}")
     public void validResult(String expectedResult ){
         assertEquals(expectedResult, match.getResult());
+    }
+
+    @Given("new scoreboard")
+    public void newScoreBoard(){
+        this.scoreBoard = new ScoreBoard();
+    }
+
+    @When("I add match to scoreboard {string} - {string}")
+    public void addMatch(String home, String away ){
+        try {
+            scoreBoard.addMatch(home, away);
+        } catch (Exception e) {
+            thrownException = e;
+        }
+    }
+
+    @Then("scoreboard size is {int}")
+    public void scoreboardSize(int expectedSize){
+        assertEquals(expectedSize, scoreBoard.size());
+    }
+
+    @Then("It should throw an {string} - {string}")
+    public void itShouldThrowAn(String expectedException, String expectedMessage) {
+        assertNotNull(thrownException);
+        assertEquals(expectedException, thrownException.getClass().getSimpleName());
+        assertEquals(expectedMessage, thrownException.getMessage());
+        thrownException = null;
     }
 }
 
