e72a15d516a0871725fdf5d8e0c2e192d30ea007
Michał Kącki <mkacki77@gmail.com>
2025-02-02 17:35:45 +0100

    Verify match result after creation - test implementation - test failed no actual implementation.

--- src/test/java/pl/lib/scoreboard/SocreBoardSteps.java
+++ src/test/java/pl/lib/scoreboard/SocreBoardSteps.java
@@ -1,9 +1,32 @@
 package pl.lib.scoreboard;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 
 public class SocreBoardSteps {
 
-    @Given("initiation")
-    public void initiation() {}
+    String home;
+    String away;
+
+    Match match;
+
+    @Given("Two teams {string} and {string}")
+    public void selectTeams(String home, String away) {
+        this.home = home;
+        this.away = away;
+    }
+
+    @When("I create create new match")
+    public void newMatch(){
+        match = new Match(home, away);
+    }
+
+    @Then("current result of that match is {string}")
+    public void validResult(String expectedResult ){
+        assertEquals(expectedResult, match.getResult());
+    }
 }
+
--- src/test/resources/features/new_match.feature
+++ src/test/resources/features/new_match.feature
@@ -1,6 +1,6 @@
 Feature: Adding new match to scoreboard
 
-  Scenario: Add new match to scoreboard
+  Scenario: Verify match result after creation
     Given Two teams "Poland" and "United Kingdom"
-    When I create create New Match
+    When I create create new match
     Then current result of that match is "Poland 0 - United Kindgom 0"
