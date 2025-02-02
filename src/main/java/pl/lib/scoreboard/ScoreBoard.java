d962d3a5ee732c35117f138e44f04ec0f45d79f9
Michał Kącki <mkacki77@gmail.com>
2025-02-02 22:56:48 +0100

    Scoreboard implementation added Acceptance test corrected

--- src/main/java/pl/lib/scoreboard/ScoreBoard.java
+++ src/main/java/pl/lib/scoreboard/ScoreBoard.java
@@ -1,5 +1,38 @@
 package pl.lib.scoreboard;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ScoreBoard {
 
+    private static final String AWAY_TEAM_ALREADY_PLAYING = "Away team already playing another match";
+    private static final String HOME_TEAM_ALREADY_PLAYING = "Home team already playing another match";
+
+    private Set<String> playing;
+    private Map<String, Match> contents;
+
+    public ScoreBoard() {
+        playing = new HashSet<>();
+        contents = new HashMap<>();
+    }
+
+    public void addMatch(String home, String away) {
+        if (playing.contains(home)) {
+            throw new IllegalArgumentException(HOME_TEAM_ALREADY_PLAYING);
+        } else
+        if (playing.contains(away)) {
+            throw new IllegalArgumentException(AWAY_TEAM_ALREADY_PLAYING);
+        }
+
+        contents.put(home, new Match(home, away));
+
+        playing.add(home);
+        playing.add(away);
+    }
+
+    int size() {
+        return contents.size();
+    }
 }
